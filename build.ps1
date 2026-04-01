b836adfdb3149e71ffcf1edc8710197f9d2daa6b
lavedon <lavedon@gmail.com>
2026-03-31 21:10:43 -0400

    updated

--- build.ps1
+++ build.ps1
@@ -1,3 +1,7 @@
+$exeApp = "C:\tools\abs.exe"
+if([System.IO.File]::Exists($exeApp)){
+    Remove-Item $exeApp -Force
+}
 Invoke-Expression "dotnet publish -c Release --arch x64 --nologo --self-contained true"
 Copy-Item "C:\my-coding-projects\abstain\bin\Release\net10.0\win-x64\native\abstain.exe" -Destination "C:\tools" -Force
 Rename-Item "C:\tools\abstain.exe" "abs.exe" -ErrorAction SilentlyContinue
