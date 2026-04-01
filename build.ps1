f766cf83db1c8dffe397f2813ab8df6a34cbcba3
lavedon <lavedon@gmail.com>
2026-03-31 20:08:29 -0400

    project skeleton

---
+++ build.ps1
@@ -0,0 +1,7 @@
+Invoke-Expression "dotnet publish -c Release --arch x64 --nologo --self-contained true"
+Copy-Item "C:\my-coding-projects\abstain\bin\Release\net10.0\win-x64\native\abstain.exe" -Destination "C:\tools" -Force
+Rename-Item "C:\tools\abstain.exe" "abs.exe" -ErrorAction SilentlyContinue
+$path =  "C:\tools\Data\abstain.db";
+if(![System.IO.File]::Exists($path)){
+    Copy-Item "C:\my-coding-projects\abstain\Data\abstain.db" -Destination "C:\tools\Data"
+}
